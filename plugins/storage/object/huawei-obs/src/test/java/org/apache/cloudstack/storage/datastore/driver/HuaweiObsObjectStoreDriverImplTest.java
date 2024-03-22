a15add26d19fcc4f4cc911d612e3627001f53cb3
Ronny <ronald.feicht@scsynergy.de>
2024-03-22 08:45:13 +0100

    Setting CORS on bucket result: 403 <Code>AccessDenied</Code><Message>Access Denied</Message> 	wrong access key? 		switching from user to account key for CORS 			fixed test
    changed bucket policy behavior

--- plugins/storage/object/huawei-obs/src/test/java/org/apache/cloudstack/storage/datastore/driver/HuaweiObsObjectStoreDriverImplTest.java
+++ plugins/storage/object/huawei-obs/src/test/java/org/apache/cloudstack/storage/datastore/driver/HuaweiObsObjectStoreDriverImplTest.java
@@ -67,7 +67,7 @@ public class HuaweiObsObjectStoreDriverImplTest {
         Mockito.when(bucketDao.findById(Mockito.anyLong())).thenReturn(new BucketVO(0, 0, 0, bucketName, 100, false, false, false, "public"));
         Mockito.when(objectStoreVO.getUrl()).thenReturn("http://localhost:9000");
         Mockito.when(objectStoreDao.findById(Mockito.any())).thenReturn(objectStoreVO);
-        Mockito.doNothing().when(huaweiObsObjectStoreDriverImpl).cors(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyString());
+        Mockito.doNothing().when(huaweiObsObjectStoreDriverImpl).cors(Mockito.anyString(), Mockito.anyLong(), Mockito.anyString());
         Bucket bucketRet = huaweiObsObjectStoreDriverImpl.createBucket(bucket, false);
         assertEquals(bucketRet.getName(), bucket.getName());
         Mockito.verify(obsClient, Mockito.times(1)).headBucket(Mockito.anyString());
